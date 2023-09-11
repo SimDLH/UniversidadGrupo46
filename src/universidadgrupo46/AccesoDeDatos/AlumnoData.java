5ed6f049d80250902dca947c81f8cd8322a32b8f
SimDLH <simonignaciodelasheras@gmail.com>
2023-09-11 18:03:23 -0300

    intento

---
+++ src/universidadgrupo46/AccesoDeDatos/AlumnoData.java
@@ -0,0 +1,114 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package universidadgrupo46.AccesoDeDatos;
+
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+import org.mariadb.jdbc.Statement;
+import universidadgrupo46.Entidades.Alumno;
+
+/**
+ *
+ * @author simon
+ */
+public class AlumnoData {
+    private Connection con=null;
+    
+    
+    public AlumnoData(){
+        con=Conexion.getConnexion();
+        
+    }
+    
+    public void guardarAlumno(Alumno alumno){
+        String sql="insert into alumno(dni,apellido,nombre,fechaNacimiento,estado)"
+                + "values(?,?,?,?,?)";
+        try {
+            PreparedStatement ps=con.prepareStatement(sql,Statement.RETURN_GENERATED_KEYS);
+            ps.setInt(1, alumno.getDni());
+            ps.setString(2, alumno.getApellido());
+            ps.setString(3, alumno.getNombre());
+            ps.setDate(4, Date.valueOf(alumno.getFechaNac()));
+            ps.setBoolean(5, alumno.isEstado());
+            ps.executeUpdate();
+            ResultSet rs=ps.getGeneratedKeys();
+            if (rs.next()){
+                alumno.setIdAlumno(rs.getInt(1));
+                JOptionPane.showMessageDialog(null, "Alumno guardado");
+            }
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error al acceder a la tabla alumno");
+        }
+    }
+    
+   public void modificarAlumno(Alumno alumno){
+       String sql="Update alumno set dni=?,apellido=?,nombre=?,fechaNacimiento=?"
+               + "Where IdAlumno=?";
+        try {
+            PreparedStatement ps=con.prepareStatement(sql);
+            ps.setInt(1, alumno.getDni());
+            ps.setString(2, alumno.getApellido());
+            ps.setString(3, alumno.getNombre());
+            ps.setDate(4,Date.valueOf(alumno.getFechaNac()));
+            ps.setInt(5, alumno.getIdAlumno());
+            int exito=ps.executeUpdate();
+            if (exito==1){
+                JOptionPane.showMessageDialog(null, "Alumno modificado");
+            }
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error al acceder a la tabla de alumno");
+        }
+       
+   }
+   
+   public void eliminarAlumno(int id){
+       String sql="Update alumno set estado=0 where IdAlumno=?";
+        try {
+            PreparedStatement ps=con.prepareStatement(sql);
+            ps.setInt(1, id);
+            int exito=ps.executeUpdate();
+            if (exito==1){
+                JOptionPane.showMessageDialog(null, "Alumno eliminado");
+            }
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error al acceder a la tabla alumno");
+        }
+       
+       
+   }
+   
+   public Alumno buscarAlumno(int id){
+
+       String sql="SELECT dni,apellido,nombre,fechaNacimiento From alumno where IdAlumno=? AND estado=1";
+       Alumno alumno=null;
+        try {
+            PreparedStatement ps=con.prepareStatement(sql);
+            ps.setInt(1, id);
+
+            ResultSet rs=ps.executeQuery();
+            if (rs.next()){
+                alumno=new Alumno();
+                alumno.setIdAlumno(id);
+                alumno.setDni(rs.getInt("dni"));
+                alumno.setApellido(rs.getString("apellido"));
+                alumno.setNombre(rs.getString("nombre"));
+                alumno.setFechaNac(rs.getDate("fechaNacimiento").toLocalDate());
+                alumno.setEstado(true);
+            }else{
+                JOptionPane.showMessageDialog(null, "El alumno no existe");
+            }
+            ps.close();
+
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error al acceder a la tabla de alumno");
+        }
+        return alumno;
+   }
+}
--- src/universidadgrupo46/UniversidadGrupo46.java
+++ src/universidadgrupo46/UniversidadGrupo46.java
@@ -6,7 +6,11 @@
 package universidadgrupo46;
 
 import java.sql.Connection;
+import java.time.LocalDate;
+import java.time.Month;
+import universidadgrupo46.AccesoDeDatos.AlumnoData;
 import universidadgrupo46.AccesoDeDatos.Conexion;
+import universidadgrupo46.Entidades.Alumno;
 
 /**
  *
@@ -20,6 +24,21 @@ public class UniversidadGrupo46 {
     public static void main(String[] args) {
         // TODO code application logic here
         Connection con=Conexion.getConnexion();
+        //Alumno al1=new Alumno(32864391,"Tomas","Juan",LocalDate.of(1991, 12, 17),true);
+        AlumnoData aluD=new AlumnoData();
+        //aluD.guardarAlumno(al1);
+        //aluD.modificarAlumno(al1);
+        //aluD.eliminarAlumno(1);
+        Alumno alumnoencontrado=aluD.buscarAlumno(4);
+        if(alumnoencontrado!=null){
+        System.out.println("dni "+alumnoencontrado.getDni());
+        System.out.println("Apellido "+alumnoencontrado.getApellido());
+        System.out.println("nombre "+alumnoencontrado.getNombre());
+        System.out.println("Fecha de nacimiento "+alumnoencontrado.getFechaNac());
+        }
     }
+    
+    
+    
     
 }
